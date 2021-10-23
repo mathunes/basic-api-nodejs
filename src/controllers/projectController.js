b0c9239218cefabd639a9e917b7844cece26d42f
mathunes <matheusantunes720@gmail.com>
2021-10-23 17:04:11 -0300

    first commit

---
+++ src/controllers/projectController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/', (req, res) => {
+    res.send({ id: req.userId });
+});
+
+module.exports = app => app.use('/projects', router);
